66596330490c541858fa80e4f44948aef276f131
afeiship <1290657123@qq.com>
2021-01-30 14:04:42 +0800

    feat: use jswork new plugin

--- dist/index.js
+++ dist/index.js
@@ -4,24 +4,42 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
-
-var STYLE = { fontStyle: 'italic' };
+var _slateHyperscript = require('slate-hyperscript');
 
 exports.default = {
-  renderMark: function renderMark(inProps, inEditor, inNext) {
-    var children = inProps.children,
-        attributes = _objectWithoutProperties(inProps, ['children']);
+  name: 'italic',
+  importer: function importer(el, children) {
+    var nodeName = el.nodeName.toLowerCase();
+    if (nodeName === 'span' && el.style.fontStyle === 'italic') {
+      return (0, _slateHyperscript.jsx)('text', { italic: true }, children);
+    }
+  },
+  // to-html
+  exporter: function exporter(node, children) {
+    if (!children) {
+      if (node.italic) {
+        return '<span style="font-style: italic">' + node.text + '</span>';
+      }
+    }
+  },
+  hooks: {
+    leaf: function leaf(inContext, _ref) {
+      var attributes = _ref.attributes,
+          children = _ref.children,
+          _leaf = _ref.leaf;
 
-    switch (inProps.mark.type) {
-      case 'italic':
-        return React.createElement(
-          'span',
-          { style: STYLE },
+      if (_leaf.italic) {
+        children = React.createElement(
+          'em',
+          null,
           children
         );
-      default:
-        return inNext();
+      }
+      return React.createElement(
+        'span',
+        attributes,
+        children
+      );
     }
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-const STYLE = { fontStyle: 'italic' };
+import { jsx } from 'slate-hyperscript';
 
 export default {
-  renderMark(inProps, inEditor, inNext) {
-    const { children, ...attributes } = inProps;
-    switch (inProps.mark.type) {
-      case 'italic':
-        return (
-          <span style={STYLE}>
-            {children}
-          </span>
-        );
-      default:
-        return inNext();
+  name: 'italic',
+  importer: (el, children) => {
+    const nodeName = el.nodeName.toLowerCase();
+    if (nodeName === 'span' && el.style.fontStyle === 'italic') {
+      return jsx('text', { italic: true }, children);
+    }
+  },
+  // to-html
+  exporter: (node, children) => {
+    if (!children) {
+      if (node.italic) {
+        return `<span style="font-style: italic">${node.text}</span>`;
+      }
+    }
+  },
+  hooks: {
+    leaf: (inContext, { attributes, children, leaf }) => {
+      if (leaf.italic) {
+        children = <em>{children}</em>;
+      }
+      return <span {...attributes}>{children}</span>;
     }
   }
 };
