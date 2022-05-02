cf200acd14e323209bf4210e81f0c4a53aa9d80c
afeiship <1290657123@qq.com>
2022-05-02 18:09:55 +0800

    chore: upt api test

--- __tests__/use-counter.spec.ts
+++ __tests__/use-counter.spec.ts
@@ -20,4 +20,31 @@ describe('useCounter()', () => {
     act(() => result.current.setCount(-5));
     expect(result.current.count).toBe(-5);
   });
+
+  test('api: increment', () => {
+    const { result } = renderHook(() => useCounter(0));
+    expect(result.current.count).toBe(0);
+    act(() => result.current.increment());
+    expect(result.current.count).toBe(1);
+    act(() => result.current.increment());
+    expect(result.current.count).toBe(2);
+  });
+
+  test('api: decrement', () => {
+    const { result } = renderHook(() => useCounter(0));
+    expect(result.current.count).toBe(0);
+    act(() => result.current.decrement());
+    expect(result.current.count).toBe(-1);
+    act(() => result.current.decrement());
+    expect(result.current.count).toBe(-2);
+  });
+
+  test('api: reset', () => {
+    const { result } = renderHook(() => useCounter(0));
+    expect(result.current.count).toBe(0);
+    act(() => result.current.setCount(10));
+    expect(result.current.count).toBe(10);
+    act(() => result.current.reset());
+    expect(result.current.count).toBe(0);
+  });
 });
