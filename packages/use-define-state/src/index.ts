891fc5eae5878da522fe42015dcf7f7227546543
afeiship <1290657123@qq.com>
2023-07-31 22:59:52 +0800

    feat: add use define state

---
+++ packages/use-define-state/src/index.ts
@@ -0,0 +1,68 @@
+import createProxy from '@jswork/create-proxy';
+import { useState } from 'react';
+
+// https://chat.zhile.io/c/b65e7f07-bab8-49de-9b47-ad422d390b33
+
+declare var wx: any;
+
+export interface UseDefineStateOptions {
+  state?: any;
+  getters?: Record<string, Function>;
+  actions?: Record<string, Function>;
+}
+
+const useDefineState = (initialState: UseDefineStateOptions) => {
+  const [state, setState] = useState(initialState.state);
+
+  // Define actions
+  const actions = initialState.actions || {};
+
+  if (initialState.actions) {
+    for (const [actionName, actionFunction] of Object.entries(initialState.actions)) {
+      // Check if the actionFunction is an arrow function
+      if (actionFunction instanceof Function && !actionFunction.hasOwnProperty('prototype')) {
+        // If it's an arrow function, just pass the state as the first parameter
+        actions[actionName] = (...args) => actionFunction(state, ...args);
+      } else {
+        // If it's a non-arrow function, bind the state to 'this' and pass it as the first parameter
+        actions[actionName] = actionFunction.bind(state);
+      }
+    }
+  }
+
+  // Deep proxy to handle nested objects
+  const deepProxyState = createProxy(state, (target, key, value) => {
+    setState((prevState) => {
+      const updatedState = { ...prevState };
+      let parent = updatedState;
+      const keys = key.split('.');
+      for (let i = 0; i < keys.length - 1; i++) {
+        if (!parent[keys[i]]) parent[keys[i]] = {};
+        parent = parent[keys[i]];
+      }
+      parent[keys[keys.length - 1]] = value;
+      return updatedState;
+    });
+  });
+
+  // getters
+  if (initialState.getters) {
+    for (const [getterName, getterFunction] of Object.entries(initialState.getters)) {
+      Object.defineProperty(state, getterName, {
+        get: () => getterFunction(state),
+      });
+    }
+  }
+
+  return {
+    state: deepProxyState,
+    ...actions,
+  };
+};
+
+// for commonjs es5 require
+if (typeof module !== 'undefined' && module.exports && typeof wx === 'undefined') {
+  module.exports = useDefineState;
+}
+
+export default useDefineState;
